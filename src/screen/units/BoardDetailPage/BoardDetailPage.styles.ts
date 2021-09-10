dbc86d6f0f06d79309f76dd9d8dc928aa3679a5f
김경호 <kyeongho94@gmail.com>
2021-09-10 22:00:23 +0900

    0910_kkh_detailalarmscrap

--- src/screen/units/BoardDetailPage/BoardDetailPage.styles.ts
+++ src/screen/units/BoardDetailPage/BoardDetailPage.styles.ts
@@ -1,21 +1,155 @@
 import styled from '@emotion/native';
 
-export const Wrapper = styled.View`
-  width: 330px;
-  height: 464px;
-`;
-export const Head = styled.View`
-  width: 100%;
-  height: 100px;
-`;
-export const Body = styled.View`
-  width: 100%;
-  height: 194px;
-`;
-export const Title = styled.TextInput``;
+export const Wrapper = styled.View``;
 
-export const Contents = styled.Text`
-  font-size: 25px;
+export const BackImage = styled.View`
+  width: 100%;
+
+  background-color: #FFBE2B;
 `;
 
-export const Button = styled.TouchableOpacity``;
+export const DetailWrapper = styled.View`
+  width: 100%;
+  /* height: 1413px; */
+  background-color: #fff;
+  margin-top: 30px;
+  padding: 0 20px;
+  border-radius: 30px 30px 0 0;
+`
+
+export const Back = styled.Image`
+  width: 6px;
+  height: 10px;
+  margin: 30px 0 17px 0;
+`
+
+export const Top = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 11px;
+`
+
+export const Title = styled.Text`
+  font-size: 18px;
+  line-height: 20px;
+  /* letter-spacing: -0.02; */
+  font-weight: 700;
+`
+
+export const TopLeft = styled.View``
+
+export const TopRight = styled.View`
+  display: flex;
+  flex-direction: row;
+`
+
+// export const Edit = styled.Image`
+//   width: 14px;
+//   height: 14px;
+//   margin-right: 10px;
+// `
+
+
+// export const Delete = styled.Image`
+//   width: 13px;
+//   height: 15px;
+// `
+
+export const Scrap = styled.Image`
+  width: 12px;
+  height: 18px;
+`
+
+export const UserInfo = styled.View`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 14.98px;
+`
+
+export const Avatar = styled.Image`
+  width: 24px;
+  height: 24px;
+  margin-right: 7px;
+  border-radius: 50px 50px 50px 50px;
+`
+
+export const UserInfoRight = styled.View`
+  display: flex;
+`
+
+export const Name = styled.Text`
+  font-size: 11px;
+  line-height: 13px;
+  /* letter-spacing: -0.02; */
+`
+
+export const EnrollmentDate = styled.Text`
+  font-size: 7.6px;
+  line-height: 11px;
+  color: rgba(0, 0, 0, 0.5);
+`
+
+export const City = styled.View`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 8px;
+`
+
+export const CityLeft = styled.View`
+  display: flex;
+  flex-direction: row;
+`
+
+export const LocationIcon = styled.Image`
+  width: 11.67px;
+  height: 11.33px;
+  margin-right: 6px;
+`
+
+export const CityName = styled.Text`
+  font-size: 10px;
+  line-height: 12px;
+  color: #777;
+`
+
+export const CityRight = styled.View`
+  margin-top: 0.7px;
+`
+
+export const TravelDate = styled.Text`
+  font-size: 9px;
+  line-height: 11px;
+  /* letter-spacing: -0.02; */
+  color: #777;
+`
+
+export const Bar = styled.View`
+  border: 1px solid #a5a5a5;
+  opacity: 0.5;
+  margin-bottom: 14px;
+`
+
+export const TravelImage = styled.Image`
+  width: 100%;
+  height: 248px;
+  margin-bottom: 40px;
+`
+
+export const TravelContents = styled.Text`
+  font-size: 12px;
+  line-height: 16px;
+  /* letter-spacing: -0.02; */
+  margin-bottom: 40px;
+  font-weight: 700;
+`
+
+export const TravelMap = styled.View`
+  width: 100%;
+  height: 230px;
+  background-color: #FFBE2B;
+  margin-bottom: 60px;
+`
+
--- src/screen/units/CommentAlarmPage/CommentAlarmPage.styles.ts
+++ src/screen/units/CommentAlarmPage/CommentAlarmPage.styles.ts
@@ -1,21 +1,74 @@
 import styled from '@emotion/native';
 
 export const Wrapper = styled.View`
-  width: 330px;
-  height: 464px;
-`;
-export const Head = styled.View`
   width: 100%;
-  height: 100px;
-`;
-export const Body = styled.View`
-  width: 100%;
-  height: 194px;
-`;
-export const Title = styled.TextInput``;
-
-export const Contents = styled.Text`
-  font-size: 25px;
+  height: 640px;
 `;
 
-export const Button = styled.TouchableOpacity``;
+export const Top = styled.View`
+  /* display: flex; */
+  flex-direction: row;
+  margin: 22px 0 20px 0;
+`;
+
+export const Back = styled.Image`
+  width: 9px;
+  height: 15px;
+  margin-left: 20px;
+`;
+
+export const TopTitle = styled.Text`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  /* letter-spacing: -0.02; */
+  margin-left: 35.2%;
+`;
+
+export const Bar = styled.View`
+  border: 1px solid #a5a5a5;
+  opacity: 0.5;
+  margin-bottom: 14px;
+`
+
+export const Notice = styled.View`
+  /* display: flex; */
+  flex-direction: row;
+  margin: 20px;
+  justify-content: space-between;
+`
+
+export const NoticeImage = styled.Image`
+  width: 20px;
+  height: 17.33px;
+  margin-top: 5px;
+`
+
+export const Contents = styled.View`
+  width: 293px;
+`
+
+export const UserComment = styled.Text`
+  font-weight: bold;
+  font-size: 13.5px;
+  line-height: 23px;
+  margin-bottom: 5px;
+`
+
+export const Preview = styled.Text`
+  font-size: 12px;
+  line-height: 16px;
+  color: #A5A5A5;
+`
+
+export const Delete = styled.Image`
+  width: 15px;
+  height: 15px;
+  margin-top: 5px;
+`
+
+export const Bar2 = styled.View`
+  border: 1px solid #d5d5d5;
+  opacity: 0.3;
+  margin-bottom: 14px;
+`
