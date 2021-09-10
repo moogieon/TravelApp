d50258646dcdfe30f141e067148a9668c97ef726
moogieon <vzlzydn5007@gmail.com>
2021-09-10 20:56:15 +0900

    09_10_LCM_Claender

--- src/screen/commons/Claender/Claender.presenter.tsx
+++ src/screen/commons/Claender/Claender.presenter.tsx
@@ -1,85 +1,47 @@
-import React, { Component } from 'react'
-import {View,ToastAndroid} from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
-import {LocaleConfig} from 'react-native-calendars';
-import { black } from 'react-native-paper/lib/typescript/styles/colors';
-import  Icon  from 'react-native-vector-icons/Ionicons';
-
-LocaleConfig.locales['fr'] = {
-  monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
-  monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin','Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
-  dayNames: ['일요일','월요일', '화요일','수요일','목요일','금요일','토요일'],
-  dayNamesShort: ['일', '월','화','수','목','금','토'],
-  today: 'Aujourd\'hui'
-};
-LocaleConfig.defaultLocale = 'fr';
-
-
-
-function CalendarPage() {
- 
-     return (
-      <View style={{backgroundColor:"black",width:500,height:500}}>
-       <Calendar
-  // 처음으로 선택되어질 날짜(현재날짜), Default = Date()
-  current={'2020-08-25'}
-  // 선택이 가능한 최소 날짜, 이 날짜 이전은 회색날짜로 표시됨, Default = undefined
-  minDate={'2020-08-01'}
-  // 선택이 가능한 최대 날짜, 이 날짜 이후는 회색날짜로 표시됨, Default = undefined
-  maxDate={'2020-08-31'}
-  // 날짜를 눌렀을 때 실행되는 함수, Default = undefined
-  onDayPress={(day) => {console.log('selected day', day)}}
-  // 날짜를 길게 눌렀을 때 실행되는 함수, Default = undefined
-  onDayLongPress={(day) => {console.log('selected day', day)}}
-  // 달력 제목에 표시될 월 형식, Formatting values: http://arshaw.com/xdate/#Formatting
-  monthFormat={'yyyy MM'}
-  // 달력에서 보이는 월이 바뀔때 실행되는 함수, Default = undefined
-  onMonthChange={(month) => {console.log('month changed', month)}}
-  // 달 이동 화살표 숨기기, Default = false
-  hideArrows={false}
-  // 기본 화살표를 커스텀화살표로 대체 (방향은 '왼쪽'이나 '오른쪽')
-  renderArrow={(direction) => direction === "left" ? (
-    <Icon name="chevron-back-outline" size={20} color="#50cebb" />
-      ) : (
-    <Icon name="chevron-forward-outline" size={20} color="#50cebb" />
-      )
-  }
-  // 이번 달 페이지에 다른 달 숫자를 보이지 않게 함, Default = false
-  hideExtraDays={true}
-  // day from another month that is visible in calendar page. Default = false
-  disableMonthChange={true}
-  // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-  firstDay={7}
-  // Hide day names. Default = false
-  hideDayNames={false}
-  // Show week numbers to the left. Default = false
-  showWeekNumbers={false}
-  // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-  onPressArrowLeft={subtractMonth => subtractMonth()}
-  // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-  onPressArrowRight={addMonth => addMonth()}
-  disableArrowLeft={true}
-  disableArrowRight={true}
-  // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-  disableAllTouchEventsForDisabledDays={true}
-  /** 기본 월 및 연도 제목을 사용자 지정 제목으로 바꿈. 함수는 매개변수로 day를 받는다. */
-  // 이 부분을 주석처리 해주었더니 달력이 보여졌다. 이유는 아직도 모르겠다,,
-  renderHeader={(date) => {/*Return JSX*/}}
-  markingType={'period'}
-  markedDates={{
-    '2012-05-15': {marked: true, dotColor: '#50cebb'},
-    '2012-05-16': {marked: true, dotColor: '#50cebb'},
-    '2012-05-21': {startingDay: true, color: '#50cebb', textColor: 'white'},
-    '2012-05-22': {color: '#70d7c7', textColor: 'white'},
-    '2012-05-23': {color: '#70d7c7', textColor: 'white', marked: true, dotColor: 'white'},
-    '2012-05-24': {color: '#70d7c7', textColor: 'white'},
-    '2012-05-25': {endingDay: true, color: '#50cebb', textColor: 'white'}
-  }}
-/>
-
-      </View>
-     )
-   }
+import {
+  Colum4,
+  ModalView,
+  ButtonClose,
+  Calendar,
  
 
-export default CalendarPage;
+
+ 
+} from './Caleander.styles';
+import React from 'react';
+import CalendarPicker from 'react-native-calendar-picker';
+import { Alert, Modal } from 'react-native';
+
+
+export default function ClaenderUI(props:any) {
+  return (
+    <>
+      
+
+            <Calendar>
+              <ModalView>
+         
+            <CalendarPicker
+              startFromMonday={true}
+              allowRangeSelection={true}
+              minDate={props.minDate}
+              maxDate={props.maxDate}
+              weekdays={['일', '월','화','수','목','금','토']}
+              todayBackgroundColor="#ffbf2b87"
+              selectedDayColor="#FFBE2B"
+              selectedDayTextColor="black"
+              onDateChange={props.onDateChange}
+              />
+             <ButtonClose onPress={()=>props.setShow(false)}>
+                <Colum4>
+                   확인
+                </Colum4>
+              </ButtonClose>
+              </ModalView>
+              </Calendar>
+              
+           
+          
+    </>
+  );
+}
--- src/screen/units/BoardWritePage/BoardWritePage.presenter.tsx
+++ src/screen/units/BoardWritePage/BoardWritePage.presenter.tsx
@@ -5,54 +5,81 @@ import {
   Head,
   Colum,
   Colum2,
+  Colum3,
+  Colum4,
   Body,
   Contents,
   Title,
   DatePick,
+  ModalView,
+  ButtonClose,
+  Calendar,
   BodyBox,
   WorldPick,
   LocPick,
-  Colum3,
+  IconImg
+
 
  
 } from './BoardWritePage.styles';
 import React from 'react';
 import CalendarPage from '../../commons/Claender/Claender.presenter';
-
+import CalendarPicker from 'react-native-calendar-picker';
+import { Alert, Modal } from 'react-native';
+import Claender from '../../commons/Claender/Claender.container';
 // import MapView from 'react-native-maps';
 
 export default function BoardWritePageUI(props:any) {
   return (
     <>
-       {props.show && (
-              <CalendarPage/>
-      )}
+      
       <Head>
         <Button>
         <BackIcon source={require('../../../Assets/Images/GoToBack_B.png')} />
         </Button>
        <Colum>동행찾기 글쓰기</Colum>
        <Button><Colum2>등록</Colum2></Button>
-      </Head>
+      </Head> 
+      {/* {props.show ?( */}
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={props.show}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          props.setShow(!props.show);
+        }}
+      >  
+      <Claender  setShow={props.setShow} 
+       claenderDate={props.claenderDate}  setClaenderDate={props.setClaenderDate}
+      />
+              </Modal>
+            <Wrapper> 
+              <Body>
+                <Title placeholder={"제목"}></Title>
+                <DatePick onPress={()=>props.setShow(true)} >
+                  <IconImg source={require("../../../Assets/Images/IconSchedule.png")}/>
+                  <Colum3 startDate={props.claenderDate.startDate &&props.claenderDate.endDate } >{props.claenderDate.startDate &&props.claenderDate.endDate ?props.claenderDate.startDate + '   '+"~" +'   '+ props.claenderDate.endDate: "날짜를 선택" }</Colum3>
+                </DatePick>
+  
+          <BodyBox>
+            <WorldPick>
+            <IconImg source={require("../../../Assets/Images/IconEarth.png")}/>
+              <Colum3>대륙 선택</Colum3>
+              </WorldPick>
+            <LocPick>
+            <IconImg source={require("../../../Assets/Images/IconLocationInBoard.png")}/>
+              <Colum3>위치 추가</Colum3></LocPick>
+            </BodyBox> 
+              </Body>
+              <Body>
+                <Button>
+                  <Contents></Contents>
+                </Button>
+              </Body>
+            </Wrapper>
      
-      <Wrapper>
-        <Body>
-          <Title placeholder={"제목"}></Title>
-          <DatePick onPress={props.showMode} >
-            <Colum3>{props.date}</Colum3>
-          </DatePick>
-       
-    <BodyBox>
-      <WorldPick><Colum3>대륙 선택</Colum3></WorldPick>
-      <LocPick><Colum3></Colum3></LocPick>
-      </BodyBox> 
-        </Body>
-        <Body>
-          <Button>
-            <Contents></Contents>
-          </Button>
-        </Body>
-      </Wrapper>
+
     </>
   );
 }
