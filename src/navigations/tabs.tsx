33153747d4e318485314c4f102b43c718bf499cf
stonegorilla <tjwnrhkd123456@gmail.com>
2021-09-16 11:18:08 +0900

    sjk_0916_userpageuseform

--- src/navigations/tabs.tsx
+++ src/navigations/tabs.tsx
@@ -62,7 +62,7 @@ const HomeStackScreen = () => {
     <HomeStack.Navigator>
       <HomeStack.Screen
         name="Home"
-        component={AreaPage}
+        component={MainPage}
         options={{
           title: 'Home',
           headerShown: false,
