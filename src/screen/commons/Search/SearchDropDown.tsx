49abf9fe0d7cd03e35df85472fad52cce5930e03
pyyyyye <qkzud8033@hanmail.net>
2021-09-23 18:14:32 +0900

    0923_PYE_Search finish~

--- src/screen/commons/Search/SearchDropDown.tsx
+++ src/screen/commons/Search/SearchDropDown.tsx
@@ -1,38 +1,80 @@
-// import React, { useState } from "react"
-// import { Text, View } from "react-native"
-// export default function SearchDropDown(props:any){
-// const {dataSource} = props
-//     return(
-//         <View style={{ backgroundColor: 'red'}}>
-//             {dataSource.map(item => {
-//                 return(
-//                 <Text style={{   paddingHorizontal:10, backgroundColor:'red'  
-//                 }}>
-//                  {item}
-//                 </Text>
-//                 )}
-//             )}
-//         </View>
-//     )
-// }
 
-
-import React  from "react"
-import { Text, View } from "react-native"
+import React from "react"
+import {  ScrollView } from "react-native"
+import {  Button,
+    CardWrap,
+    Card,
+    CardLeft,
+    CardRight,
+    ScrapButton,
+    CardTitle,
+    CardMiddle,
+    CardMiddleContents,
+    LocationImg,
+    CardMiddleText,
+    CardWriter,
+    WriterPhoto,
+    WriterName,
+    ImageBox,} from './Search.styles'
 
 export default function SearchDropDown(props:any){
-    return(
-        <View style={{ backgroundColor: 'red'}}>
-            {props.data.fetchBoards.map((data) => {
-                <Text 
-                key={props.data.id}
-                    style={{ 
-                        paddingHorizontal:10, backgroundColor:'red', 
-                        margin:5  }}>
-                 {data.title}
-                </Text>
-                }
-            )}
-        </View>
-    )
+ console.log('검색데이터 이거 찐' ,props.data?.fetchBoards)
+ const goToBoardDetail = id => () => {
+    props.navigation.push('BoardDetailPage', {id: id});
+  };
+ const scrapBtn = () => {};
+          return (
+            <ScrollView>
+                {props.data?.fetchBoards.map((data : any, index : any)=>(
+                    <Card key={index} id={data._id}>
+                        <CardLeft 
+                        onPress={goToBoardDetail(data._id)}
+                        >
+                        <CardTitle>{data.title.substr(0, 40) + '..'}
+                        </CardTitle>
+                        <CardMiddle>
+                            <LocationImg
+                            source={require('../../../Assets/Images/IconLocation.png')}
+                            />
+                            <CardMiddleContents>
+                            <CardMiddleText>
+                                {data.location?.area}
+                                {', '}
+                                {data.location?.country}
+                                {', '}
+                                {data.location?.city}
+                            </CardMiddleText>
+                            <CardMiddleText>
+                                {data.startDate.substr(0, 10)}{' ~ '}
+                                {data.endDate.substr(0, 10)}
+                            </CardMiddleText>
+                            </CardMiddleContents>
+                        </CardMiddle>
+                        <CardWriter>
+                            <WriterPhoto>
+                            <ImageBox
+                                source={require('../../../Assets/Images/IconUserPhoto.png')}
+                            />
+                            </WriterPhoto>
+                            <WriterName>{data.writer.name}</WriterName>
+                        </CardWriter>
+                        </CardLeft>
+                        <CardRight>
+                  <Button onPress={scrapBtn}>
+                    <ScrapButton
+                      source={require('../../../Assets/Images/IconScrap_G.png')}
+                      resizeMode="cover"
+                    />
+                  </Button>
+                </CardRight>
+                    </Card>
+                 ))}
+            </ScrollView>
+          );
+
+
+
+
+       
+    
 }
